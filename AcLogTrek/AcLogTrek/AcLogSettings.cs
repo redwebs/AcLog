a4813bb70d296c13bc093cdab0ecbe50a695a9fd
Fred Peters <fredwebs@gmail.com>
2019-10-04 16:29:27 -0400

    Changing to Json Settings.

---
+++ AcLogTrek/AcLogTrek/AcLogSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AclTrek
+{
+    public class AcLogSettings
+    {
+        public AppGeneral ApplicationSettings { get; set; }
+        public Accessmdb[] AccessMdbArray { get; set; }
+        public Sqlserver[] SqlServerArray { get; set; }
+        public Windowsuser WindowsUserSettings { get; set; }
+    }
+
+    public class AppGeneral
+    {
+        public string LogFilePath { get; set; }
+    }
+
+    public class Windowsuser
+    {
+        public string WinUser { get; set; }
+        public string WinPassWord { get; set; }
+    }
+
+    public class Accessmdb
+    {
+        public string FilePath { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string[] Table { get; set; }
+    }
+
+    public class Sqlserver
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Security { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string[] Table { get; set; }
+    }
+
+}
--- AcLogTrek/AcLogTrek/FormHelper.cs
+++ AcLogTrek/AcLogTrek/FormHelper.cs
@@ -3,189 +3,205 @@ using System.IO;
 using System.Text;
 using System.Windows.Forms;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace AclTrek
 {
-	public static class FormHelper
-	{
+    public static class FormHelper
+    {
 
-		#region XML
+        #region XML
 
-		private static string RemoveNamespace(string protocolData)
-		{
-			if (protocolData.Contains("xsi"))
-			{
-				int start = protocolData.IndexOf("xsi");
-				int length = protocolData.IndexOf(">", start);
-				protocolData = protocolData.Remove(start, length - start);
-			}
-			return protocolData;
-		}
+        private static string RemoveNamespace(string protocolData)
+        {
+            if (protocolData.Contains("xsi"))
+            {
+                int start = protocolData.IndexOf("xsi");
+                int length = protocolData.IndexOf(">", start);
+                protocolData = protocolData.Remove(start, length - start);
+            }
+            return protocolData;
+        }
 
-		public static XmlReader GetReaderAtNode(string filePath, string nodeName)
-		{
-			XmlTextReader reader = new XmlTextReader(filePath);
+        public static XmlReader GetReaderAtNode(string filePath, string nodeName)
+        {
+            XmlTextReader reader = new XmlTextReader(filePath);
 
-			while (reader.Read())
-			{
-				if (reader.NodeType == XmlNodeType.Element)
-				{
-					if (reader.Name.Equals(nodeName))
-					{
-						return reader;
-					}
-				}
-			}
-			return null;
-		}
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals(nodeName))
+                    {
+                        return reader;
+                    }
+                }
+            }
+            return null;
+        }
 
-		#endregion
+        #endregion
 
-		#region File IO
+        #region File IO
 
-		public static string ReadFile(string filePath)
-		{
-			StreamReader sr;
-			string fileText = "";
+        public static string ReadFile(string filePath)
+        {
+            StreamReader sr;
+            string fileText = "";
 
-			if (File.Exists(filePath))
-			{
-				sr = File.OpenText(filePath);
-				fileText += sr.ReadToEnd();
-				sr.Close();
-			}
-			return fileText;
-		}
+            if (File.Exists(filePath))
+            {
+                sr = File.OpenText(filePath);
+                fileText += sr.ReadToEnd();
+                sr.Close();
+            }
+            return fileText;
+        }
 
-		public static void StringToFile(StringBuilder saveData, string filePath)
-		{
-			if (filePath.Length.Equals(0)) return;
+        public static void StringToFile(StringBuilder saveData, string filePath)
+        {
+            if (filePath.Length.Equals(0)) return;
 
-			// create a writer and open the file
-			TextWriter tw = new StreamWriter(filePath, false);
+            // create a writer and open the file
+            TextWriter tw = new StreamWriter(filePath, false);
 
-			// write a line of text to the file
-			tw.WriteLine(saveData.ToString());
+            // write a line of text to the file
+            tw.WriteLine(saveData.ToString());
 
-			// close the stream
-			tw.Close();
-		}
+            // close the stream
+            tw.Close();
+        }
 
-		public static void CopyToClipboard(string clipInfo)
-		{
-			System.Windows.Forms.Clipboard.SetDataObject(clipInfo, true);
-		}
+        public static void CopyToClipboard(string clipInfo)
+        {
+            System.Windows.Forms.Clipboard.SetDataObject(clipInfo, true);
+        }
 
-		public static string GetFromClipboard()
-		{
-			IDataObject iData = Clipboard.GetDataObject();
+        public static string GetFromClipboard()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
 
-			//Determine whether the data is in a text format.
-			if (iData.GetDataPresent(DataFormats.Text))
-			{
-				return iData.GetData(DataFormats.Text).ToString();
-			}
-			else
-			{
-				return "";
-			}
-		}
+            //Determine whether the data is in a text format.
+            if (iData.GetDataPresent(DataFormats.Text))
+            {
+                return iData.GetData(DataFormats.Text).ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
 
-		/// <summary>
-		/// Opens ofDialog to get a filename
-		/// </summary>
-		/// <param name="initialDir">The directory to start in.</param>
-		/// <param name="fileName">The default filename to be displayed.</param>
-		/// <param name="defExtension">The Default File extension without the leading period.</param>
-		/// <param name="filter">The set of pairs of filters, separated with "|". Each pair consists of a description|file spec.</param>
-		/// <param name="title">Dialog title.</param>
-		/// <param name="chkExists">If true, insures the file exists.</param>
-		/// <returns>Full path of file.</returns>
-		/// 
-		public static string GetFileName(OpenFileDialog ofDialog, string initialDir, string fileName,
-			string defExtension, string filter, string title, bool chkExists, out string error)
-		{
-			error = string.Empty;
+        /// <summary>
+        /// Opens ofDialog to get a filename
+        /// </summary>
+        /// <param name="initialDir">The directory to start in.</param>
+        /// <param name="fileName">The default filename to be displayed.</param>
+        /// <param name="defExtension">The Default File extension without the leading period.</param>
+        /// <param name="filter">The set of pairs of filters, separated with "|". Each pair consists of a description|file spec.</param>
+        /// <param name="title">Dialog title.</param>
+        /// <param name="chkExists">If true, insures the file exists.</param>
+        /// <returns>Full path of file.</returns>
+        /// 
+        public static string GetFileName(OpenFileDialog ofDialog, string initialDir, string fileName,
+            string defExtension, string filter, string title, bool chkExists, out string error)
+        {
+            error = string.Empty;
 
-			try
-			{
-				// Check to ensure that the selected file exists.  Dialog box displays a warning otherwise.
+            try
+            {
+                // Check to ensure that the selected file exists.  Dialog box displays a warning otherwise.
 
-				ofDialog.InitialDirectory = initialDir;
-				ofDialog.FileName = fileName;
-				ofDialog.CheckFileExists = chkExists;
+                ofDialog.InitialDirectory = initialDir;
+                ofDialog.FileName = fileName;
+                ofDialog.CheckFileExists = chkExists;
 
-				ofDialog.DefaultExt = defExtension;
+                ofDialog.DefaultExt = defExtension;
 
-				// Return the file linked to the LNK file.
+                // Return the file linked to the LNK file.
 
-				ofDialog.DereferenceLinks = true;
-				ofDialog.Filter = filter;
-				ofDialog.Multiselect = false;
+                ofDialog.DereferenceLinks = true;
+                ofDialog.Filter = filter;
+                ofDialog.Multiselect = false;
 
-				// Restore the original directory when done selecting a file? 
-				//  False: the current directory changes
-				//	True:  put the current folder back to original
+                // Restore the original directory when done selecting a file? 
+                //  False: the current directory changes
+                //	True:  put the current folder back to original
 
-				ofDialog.RestoreDirectory = false;
+                ofDialog.RestoreDirectory = false;
 
-				// Show the Help button and Read-Only checkbox?
+                // Show the Help button and Read-Only checkbox?
 
-				ofDialog.ShowHelp = true;
-				ofDialog.ShowReadOnly = false;
+                ofDialog.ShowHelp = true;
+                ofDialog.ShowReadOnly = false;
 
-				ofDialog.Title = title;
+                ofDialog.Title = title;
 
-				// Only accept valid Win32 file names?
-				ofDialog.ValidateNames = true;
+                // Only accept valid Win32 file names?
+                ofDialog.ValidateNames = true;
 
-				if (ofDialog.ShowDialog() == DialogResult.OK)
-				{
-					return ofDialog.FileName;
-				}
-			}
-			catch (Exception ex)
-			{
-				error = "Cannot open file: " + ex.Message;
-			}
+                if (ofDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return ofDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                error = "Cannot open file: " + ex.Message;
+            }
 
-			return string.Empty;
-		}
+            return string.Empty;
+        }
 
-		/// <summary>
-		/// Opens fbDialog to get a folder name
-		/// </summary>
-		/// <param name="initialDir">Directory to select in the tree.</param>
-		/// <param name="title">Dialog title.</param>
-		/// <returns>Path of folder.</returns>
-		/// 
-		public static string GetFolderName(FolderBrowserDialog fbDialog, string initialDir, string title, out string error)
-		{
-			error = string.Empty;
+        /// <summary>
+        /// Opens fbDialog to get a folder name
+        /// </summary>
+        /// <param name="initialDir">Directory to select in the tree.</param>
+        /// <param name="title">Dialog title.</param>
+        /// <returns>Path of folder.</returns>
+        /// 
+        public static string GetFolderName(FolderBrowserDialog fbDialog, string initialDir, string title, out string error)
+        {
+            error = string.Empty;
 
-			try
-			{
-				// Use this to force selection at or below a system type folder
-				// FolderBrowserDialogEx.SetRootFolder(fbDialog , FolderBrowserDialogEx.CsIdl.AppData);
+            try
+            {
+                // Use this to force selection at or below a system type folder
+                // FolderBrowserDialogEx.SetRootFolder(fbDialog , FolderBrowserDialogEx.CsIdl.AppData);
 
-				fbDialog.ShowNewFolderButton = true;
-				fbDialog.Description = title;
-				fbDialog.SelectedPath = initialDir;
+                fbDialog.ShowNewFolderButton = true;
+                fbDialog.Description = title;
+                fbDialog.SelectedPath = initialDir;
 
-				if (fbDialog.ShowDialog() == DialogResult.OK)
-				{
-					return fbDialog.SelectedPath;
-				}
+                if (fbDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return fbDialog.SelectedPath;
+                }
 
-			}
-			catch (Exception ex)
-			{
-				error = "Error browsing folders: " + ex.Message;
-			}
+            }
+            catch (Exception ex)
+            {
+                error = "Error browsing folders: " + ex.Message;
+            }
 
-			return string.Empty;
-		}
+            return string.Empty;
+        }
 
+        public static AcLogSettings ReadSettingsJson(string acLogSettingsFilePath)
+        {
+            try
+            {
+                var json = System.IO.File.ReadAllText(acLogSettingsFilePath);
+                return JsonConvert.DeserializeObject<AcLogSettings>(json);
+
+            }
+            catch (Exception ex)
+            {
+                
+                //_Log.Error($"ReadOfficeJson: Cannot read json file at location {acLogSettingsFilePath}, ex: {ex}");
+                return null;
+            }
+        }
 
         #endregion File IO
 
